1345919a04ae522e836c9be1b0e3c1fe0e264fb5
Sophia <Sophia>
2024-03-22 18:38:52 +0800

    python_note

--- practice/input_exercise2.py
+++
@@ -1,6 +0,0 @@
-name = input("商品名称：")
-price = float(input("单价:"))
-weight = float(input("重量："))
-amount = 0.00
-amount = price * weight
-print("%s单价%.2f元/斤，购买了%.2f斤，需要支付%.2f元" % (name, price, weight, amount))
